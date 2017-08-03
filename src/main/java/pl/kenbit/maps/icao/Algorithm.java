f86e1b4b0bca585afcce1f51ce9f614c5c123fbb
M.Ziolkowski <ziolek0321@gmail.com>
2017-08-03 15:20:08 +0200

    V 1.0

---
+++ src/main/java/pl/kenbit/maps/icao/Algorithm.java
@@ -0,0 +1,43 @@
+package pl.kenbit.maps.icao;
+
+public class Algorithm {
+
+    Data data;
+
+//    String[] swap() {
+//        data = new Data();
+////        String word = data.setWord();
+//        char[] swapStringToArary = word.toCharArray();
+//        char[] letters = data.letter();
+//        String[] alphabetICAO = data.alphabetICAO();
+//        String[] finalWord = new String[word.length()];
+//        System.out.println("finall " + finalWord.length + " alpha " + alphabetICAO.length + " letters " + letters.length);
+//
+//        for (int i = 0; i <= swapStringToArary.length - 1; i++) {
+//            for (int j = 0; j <= letters.length - 1; j++) {
+//                if (swapStringToArary[i] == letters[j]) {
+//                    finalWord[i] = alphabetICAO[j];
+//                    break;
+//                } else {
+//                	
+//                }
+//            }
+//        }
+//        return finalWord;
+//    }
+    
+    
+
+//    void display() {
+//        String[] finalWord = swap();
+//        System.out.println(" ");
+//            for (int i = 0; i < finalWord.length; i++) {
+//                 System.out.print(" - " + finalWord[i]);
+//            }
+//            System.out.println(" - ");
+//    }
+//    public static void main(String[] args) {
+//        Algorithm alg = new Algorithm();
+//        alg.display();
+//    }
+}
---
+++ src/main/java/pl/kenbit/maps/icao/Data.java
@@ -0,0 +1,38 @@
+package pl.kenbit.maps.icao;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Data {
+
+//    private String word;
+
+//    public String setWord() {
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("Wpisz słowo");
+//        return word = scan.next();
+//    }
+
+    public char[] letter() {
+        char[] letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                          'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                          'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+                          'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 
+                          '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+        return letters;
+
+    }
+    
+     public String[] alphabetICAO() {
+        String[] alphabetICAO = {"alfa", "beta", "charlie", "delta", "echo", "foxtrot", "golf", "hotel", "india", "juliett", "kilo", "lima",
+            "mike", "november", "oscar", "papa", "quebec", "romeo", "sierra", "tango", "uniform", "victor", "whiskey", "x-ray", "yankee", "zulu",
+            "ALFA", "BETA", "CHARLIE", "DELTA", "ECHO", "FOXTROT", "GOLF", "HOTEL", "INDIA", "JULIETT", "KILO", "LIMA",
+            "MIKE", "NOVEMBER", "OSCAR", "PAPA", "QUEBEC", "ROMEO", "SIERRA", "TANGO", "UNIFORM", "VICTOR", "WHISKY", "X-RAY", "YANKEE", "ZULU",
+            "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        return alphabetICAO;
+
+    }
+
+}
